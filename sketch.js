1bc36ee22275547263690bd8350b58d7e64eee3d
dantonkaviya <77843323+dantonkaviya@users.noreply.github.com>
2021-04-03 17:57:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setup() {
 	
 	rope1=new rope(bob1.body,roofObject.body,-80, 0)
 	rope2=new rope(bob2.body,roofObject.body,-40, 0)
-	rope3=new rope(bob3.body,roofobject.body,0,0)
+	rope3=new rope(bob3.body,roofObject.body,0, 0)
 	rope4=new rope(bob4.body,roofObject.body,40, 0)
 	rope5=new rope(bob5.body,roofObject.body,80, 0)
 	
@@ -33,6 +33,7 @@ function setup() {
 	
   
 }
+
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
@@ -63,15 +64,4 @@ function draw() {
   
  
 }
-
-
-
-  
- 
-
-
-
-
-
-
 
